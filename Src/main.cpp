153610661e2071ce39ac2c910ef4eef561fcf313
mloell2s <marius.loellgen@smail.inf.h-brs.de>
2021-07-03 19:15:33 +0200

    #6 will play a song now

--- Src/main.cpp
+++ Src/main.cpp
@@ -4,6 +4,8 @@
 #include "VirtualTouchButton.h"
 #include "Observer.h"
 #include "SoundPlayer.h"
+#include "Song.h"
+#include "SongPlayer.h"
 
 //*******************************************************************
 
@@ -16,8 +18,8 @@
 #endif
 
 //*******************************************************************
-cTaskHandler taskHandler(&timer);
-SoundPlayer sp(taskHandler, 1.0/timer.getCycleTime() * 1000000 , dacA);
+cTaskHandler taskHandler(&timerSound);
+SoundPlayer sp(taskHandler, 1.0/timerSound.getCycleTime() * 1000000 , dacA);
 
 //*******************************************************************
 int main(void)
@@ -26,24 +28,69 @@ int main(void)
     disp.setBackColor(cHwDisplayGraphic::Navy);
     disp.clear();
   #endif
-	sp.setVolume(70);
+	
+	Song song;
+	
+	song.add(new SongItem(new Tone(C, 1)));
+	song.add(new SongItem(new Tone(D, 1)));
+	song.add(new SongItem(new Tone(E, 1)));
+	song.add(new SongItem(new Tone(F, 1)));
+	song.add(new SongItem(new Tone(G, 1)));
+	song.add(new SongItem(new Tone(G, 1)));
+	song.add(new SongItem(new Tone(A, 1)));
+	song.add(new SongItem(new Tone(A, 1)));
+	song.add(new SongItem(new Tone(A, 1)));
+	song.add(new SongItem(new Tone(A, 1)));
+	song.add(new SongItem(new Tone(G, 1)));
+	song.add(new SongItem(new Tone(A, 1)));
+	song.add(new SongItem(new Tone(A, 1)));
+	song.add(new SongItem(new Tone(A, 1)));
+	song.add(new SongItem(new Tone(A, 1)));
+	song.add(new SongItem(new Tone(G, 1)));
+	song.add(new SongItem(new Tone(F, 1)));
+	song.add(new SongItem(new Tone(F, 1)));
+	song.add(new SongItem(new Tone(F, 1)));
+	song.add(new SongItem(new Tone(F, 1)));
+	song.add(new SongItem(new Tone(E, 1)));
+	song.add(new SongItem(new Tone(E, 1)));
+	song.add(new SongItem(new Tone(D, 1)));
+	song.add(new SongItem(new Tone(D, 1)));
+	song.add(new SongItem(new Tone(D, 1)));
+	song.add(new SongItem(new Tone(D, 1)));
+	song.add(new SongItem(new Tone(C, 1)));
+	
+	cHwTimer_N   timerTakt (cHwTimer_N::TIM_0,   SongPlayer::calcCycleTime(120)); // 120 bpm
+	cTaskHandler playerHadler(&timerTakt);
+	
+	SongPlayer player(playerHadler, &timerTakt, &sp);
+	player.setSong(&song);
+	player.start();
+	
+	
   disp.printf(0,0,0,__DATE__ " " __TIME__);
-	sp.setTone(SoundPlayer::A, 1);
-	disp.printf(1,0,0,sp.getTone());
+	
+	disp.printf(1,0,0,sp.getTone()->toString());
+	int volume = 70;
   while(1)
   {
+		disp.printf(1,0,20,"Volume: %d",volume);
+		disp.printf(2,0,0,player.getCurrentTone()->toString());
+		if(btn.get())
+		{
+			disp.printf(5,0,0,"hi");
+			//start song
+		}
+			
 		//use joystick to adjust tone
 		switch( enc.get() )
     {
 				case cDevControlEncoder::LEFT:
-					disp.printf(1,0,0,sp.decrementTone());
+					sp.setVolume(volume--);
 					break;
         case cDevControlEncoder::RIGHT:
-					disp.printf(1,0,0,sp.incrementTone());
+					sp.setVolume(volume++);
 					break;
-        case cDevControlEncoder::CTRL_DWN: 
-					sp.setTone(SoundPlayer::A, 1); 
-					disp.printf(1,0,0,sp.getTone()); 
+        case cDevControlEncoder::CTRL_DWN:
 					break;
         default:
 					break;
